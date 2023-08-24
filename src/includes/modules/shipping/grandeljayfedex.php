d9362f310ad09a0c0a589bc8efde47a64815b82c
grandeljay <github@grandels.email>
2023-08-24 12:39:02 +0200

    chore(module): update to v0.5.5

--- src/includes/modules/shipping/grandeljayfedex.php
+++ src/includes/modules/shipping/grandeljayfedex.php
@@ -19,7 +19,7 @@ class grandeljayfedex extends StdModule
 {
     private Installer $installer;
 
-    public const VERSION     = '0.5.4';
+    public const VERSION     = '0.5.5';
     public array $properties = array();
 
     public static function weight(): string
