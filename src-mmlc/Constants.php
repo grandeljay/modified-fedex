3a6c7001bd939c3e11de4be46545d5c1623ebdc3
grandeljay <github@grandels.email>
2024-09-03 08:51:58 +0200

    refactor: use snake case directory name

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -6,14 +6,17 @@ class Constants
 {
     public const MODULE_SHIPPING_NAME = 'MODULE_SHIPPING_GRANDELJAYFEDEX';
 
-    public const API_ENDPOINT                = HTTPS_SERVER . '/api/grandeljay/fedex/v1';
-    public const API_ENDPOINT_WEIGHT         = self::API_ENDPOINT . '/weight';
-    public const API_ENDPOINT_WEIGHT_ADD     = self::API_ENDPOINT_WEIGHT . '/add.php';
-    public const API_ENDPOINT_WEIGHT_GET     = self::API_ENDPOINT_WEIGHT . '/get.php';
+    public const API_ENDPOINT = HTTPS_SERVER . '/api/grandeljay/fedex/v1';
+
+    public const API_ENDPOINT_WEIGHT     = self::API_ENDPOINT . '/weight';
+    public const API_ENDPOINT_WEIGHT_ADD = self::API_ENDPOINT_WEIGHT . '/add.php';
+    public const API_ENDPOINT_WEIGHT_GET = self::API_ENDPOINT_WEIGHT . '/get.php';
+
     public const API_ENDPOINT_SURCHARGES     = self::API_ENDPOINT . '/surcharges';
     public const API_ENDPOINT_SURCHARGES_ADD = self::API_ENDPOINT_SURCHARGES . '/add.php';
     public const API_ENDPOINT_SURCHARGES_GET = self::API_ENDPOINT_SURCHARGES . '/get.php';
-    public const API_ENDPOINT_PICK_PACK      = self::API_ENDPOINT . '/pick-pack';
-    public const API_ENDPOINT_PICK_PACK_ADD  = self::API_ENDPOINT_PICK_PACK . '/add.php';
-    public const API_ENDPOINT_PICK_PACK_GET  = self::API_ENDPOINT_PICK_PACK . '/get.php';
+
+    public const API_ENDPOINT_PICK_PACK     = self::API_ENDPOINT . '/pick_pack';
+    public const API_ENDPOINT_PICK_PACK_ADD = self::API_ENDPOINT_PICK_PACK . '/add.php';
+    public const API_ENDPOINT_PICK_PACK_GET = self::API_ENDPOINT_PICK_PACK . '/get.php';
 }
